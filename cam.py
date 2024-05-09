446a886e84082c5f866d7ebf82fa12196cdb3c19
lfb-1 <liuthomas817@gmail.com>
2024-05-09 17:55:22 -0400

    Update cam.py

--- cam.py
+++ cam.py
@@ -99,6 +99,7 @@ def plot_cam(cam, dataloader):
         input_norm *= 255
         output = 0.3 * grayscale_cam + 0.7 * input_norm
         output = np.rot90(output,3, axes=(1,2))
+        #! FLip left-right to make orientation consisent with radiology habits 
         output = np.flip(output,axis=2)
         if label == 1:
             for i in range(164):
